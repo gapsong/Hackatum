c3c9ec76712f558c7a6d258bb89460dc6c5134d2
gap <khiemtonthat@yahoo.de>
2016-11-12 15:12:13 +0100

    Zxin scanner added

--- app/src/main/java/com/a1knackiges/wena/MainActivity.java
+++ app/src/main/java/com/a1knackiges/wena/MainActivity.java
@@ -1,13 +1,26 @@
 package com.a1knackiges.wena;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button clickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clickButton = (Button) findViewById(R.id.button);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SimpleScannerActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 }
